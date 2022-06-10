bbd8513151a20ad64fe2800d52a09151b3e6e934
Daniel Mateos <dmateosc@outlook.es>
2022-06-10 19:44:52 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import fastify from "fastify";
 const server = fastify();
 
 server.get("/", async (request, reply) => {
-  reply.code(200).send({ message: "hooola2" });
+  reply.code(200).send({ message: "This one is the last version" });
 });
 
 server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
