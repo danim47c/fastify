e1b55464b4f4a0ed322bce48f800e1c94fdc73ec
Daniel Mateos <dmateosc@outlook.es>
2022-06-10 19:21:40 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const server = fastify();
 console.log(process.env)
 
 server.get("/", async (request, reply) => {
-  reply.code(200).send({ message: "Hello world!" });
+  reply.code(200).send({ message: "Hello world!", env: process.env });
 });
 
 server.listen(process.env.PORT || 8080, "0.0.0.0", (err, address) => {
